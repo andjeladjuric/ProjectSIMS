442fb891b93cc47bd09a36f39e99d9678f8bf2f8
tcxtc00 <tanjaney0502@gmail.com>
2021-03-28 15:58:34 +0200

    Izmene class dijagrama

--- HospitalService/HospitalService/obj/Debug/netcoreapp3.1/Windowss/EditPatient.g.i.cs
+++ HospitalService/HospitalService/obj/Debug/netcoreapp3.1/Windowss/EditPatient.g.i.cs
@@ -125,7 +125,7 @@ namespace HospitalService.Windowss {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/HospitalService;V1.0.0.0;component/windowss/editpatient.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/HospitalService;component/windowss/editpatient.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Windowss\EditPatient.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
