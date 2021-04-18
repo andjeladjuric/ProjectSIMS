57ef1a41ad64824f1806b5029d9d2bf1237b1f44
tcxtc00 <tanjaney0502@gmail.com>
2021-04-18 21:43:34 +0200

    Omogućena manipulacija terminima

---
+++ HospitalService/HospitalService/View/SecretaryUI/DayAppointments.xaml.cs
@@ -0,0 +1,131 @@
+﻿using Model;
+using Storage;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HospitalService.View.SecretaryUI
+{
+    /// <summary>
+    /// Interaction logic for DayAppointments.xaml
+    /// </summary>
+    public partial class DayAppointments : Window
+    {
+        public BindingList<CalendarHelper> ch { get; set; }
+        DateTime dt;
+        public DayAppointments(DateTime dt)
+        {
+            this.dt = dt;
+            RefreshTable(dt);
+            InitializeComponent();
+            DataContext = this;
+            datum.Content = dt.Date.ToShortDateString();
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                CalendarHelper o1 = (CalendarHelper)calendar.SelectedCells[0].Item;
+                int index = calendar.SelectedCells[0].Column.DisplayIndex - 1;
+                if (o1.Appointments[index].Item1 != "")
+                {
+                    Appointment a = o1.Appointments[index].Item2;
+                    MessageBoxResult m = MessageBox.Show($"Datum: {a.StartTime.Date.ToShortDateString()}\nPocetak: {a.StartTime.TimeOfDay}\nKraj: {a.EndTime.TimeOfDay}\nSala: {o1.Name}\nTip: {a.Type}\nDoktor: {a.doctor.Name} {a.doctor.Surname}\nPacijent: {a.patient.Name} {a.patient.Surname}", "Zelite Pomeriti Termin?", MessageBoxButton.YesNo);
+                    if (m == MessageBoxResult.Yes)
+                    {
+                        new PomeriTermin(a).ShowDialog();
+                        this.Close();
+
+                    }
+
+                }
+                else { MessageBox.Show("Termin je prazan!"); }
+            }
+            catch { this.Close(); }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //zakazi
+
+            CalendarHelper o1 = (CalendarHelper)calendar.SelectedCells[0].Item;
+            int index = calendar.SelectedCells[0].Column.DisplayIndex - 1;
+            DateTime start = new DateTime(dt.Year, dt.Month, dt.Date.Day, index + 8, 0, 0);
+            if (start < DateTime.Now)
+            {
+                MessageBox.Show("Datum koji ste izabrali je prosao.");
+                return;
+            }
+            else if (o1.Appointments[index].Item1 != "")
+            {
+                MessageBox.Show("Termin je zauzet");
+                return;
+            }
+
+            new ZakaziTermin(start, o1.Name).ShowDialog();
+
+            this.Close();
+        }
+
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+            CalendarHelper o1 = (CalendarHelper)calendar.SelectedCells[0].Item;
+            int index = calendar.SelectedCells[0].Column.DisplayIndex - 1;
+            if (o1.Appointments[index].Item1 == "")
+            {
+                MessageBox.Show("Termin je prazan, ne moze se obrisati");
+                return;
+            }
+            Appointment a = o1.Appointments[index].Item2;
+            new AppointmentStorage().Delete(a.Id);
+            MessageBox.Show("Uspesno ste obrisali termin");
+            this.Close();
+        }
+
+        private void RefreshTable(DateTime dt)
+        {
+            List<Appointment> temp = new AppointmentStorage().GetAll();
+            Dictionary<string, List<Appointment>> dict = new Dictionary<string, List<Appointment>>();
+            ch = new BindingList<CalendarHelper>();
+
+            foreach (Room room in new RoomFileStorage().GetAll())
+            {
+                dict[room.Id] = new List<Appointment>();
+            }
+
+            foreach (Appointment item in temp)
+            {
+                if (dt.Date == item.StartTime.Date)
+                {
+                    dict[item.room.Id].Add(item);
+                }
+            }
+
+            foreach (KeyValuePair<string, List<Appointment>> item in dict)
+            {
+                CalendarHelper tmp = new CalendarHelper(item.Key);
+
+                foreach (Appointment appointment in item.Value)
+                {
+                    int idx = appointment.StartTime.Hour - 8;
+                    tmp.Appointments[idx] = new Tuple<string, Appointment>("☑", appointment);
+                }
+                ch.Add(tmp);
+            }
+        }
+
+    }
+}
---
+++ HospitalService/HospitalService/View/SecretaryUI/PomeriTermin.xaml.cs
@@ -0,0 +1,87 @@
+﻿using Model;
+using Storage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HospitalService.View.SecretaryUI
+{
+    /// <summary>
+    /// Interaction logic for PomeriTermin.xaml
+    /// </summary>
+    public partial class PomeriTermin : Window
+    {
+        Appointment appointment;
+        public PomeriTermin(Appointment a)
+        {
+            appointment = a;
+            List<Room> romms = new RoomFileStorage().GetAll();
+
+            InitializeComponent();
+            sala.ItemsSource = romms;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (sala.SelectedItem == null || datum.Value == null || !vreme.Value.HasValue)
+            {
+                MessageBox.Show("Morate uneti sva polja");
+                return;
+            }
+
+
+            DateTime date = new DateTime(datum.Value.Value.Year, datum.Value.Value.Month, datum.Value.Value.Date.Day, vreme.Value.Value.Hour, 0, 0);
+            if (date < DateTime.Now)
+            {
+                MessageBox.Show("Datum koji ste izabrali je prosao.");
+                return;
+            }
+            else if (date.Hour < 8 || date.Hour > 21)
+            {
+                MessageBox.Show("Bolnica radi od 8h do 21h");
+                return;
+            }
+            else if (date > appointment.StartTime.AddDays(2) || date < appointment.StartTime.AddDays(-2))
+            {
+                MessageBox.Show("Mozete pomeriti termin samo za dva dana ");
+                return;
+            }
+
+            foreach (Appointment item in new AppointmentStorage().GetAll())
+            {
+                if (item.StartTime == date && item.room.Id == ((Room)sala.SelectedItem).Id)
+                {
+                    MessageBox.Show("Izabrani termin je zauzet, molimo vas izaberite drugi");
+                    return;
+                }
+                else if (item.doctor.Jmbg == appointment.doctor.Jmbg && item.StartTime == date)
+                {
+                    MessageBox.Show("Doktor je u datom terminu zauzet");
+                    return;
+                }
+                else if (item.patient.Jmbg == appointment.patient.Jmbg && item.StartTime == date)
+                {
+                    MessageBox.Show("Pacijent je u datom terminu zauzet");
+                    return;
+                }
+            }
+
+            new AppointmentStorage().Edit(appointment.Id, date, date.Add(new TimeSpan(1, 0, 0)), (Room)sala.SelectedItem);
+            MessageBox.Show("Uspesno promenjen termin");
+            this.Close();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
