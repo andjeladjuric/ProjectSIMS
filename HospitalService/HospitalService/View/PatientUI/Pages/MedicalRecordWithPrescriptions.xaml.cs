16c049226686f56bcddd3855007dea372644b53b
Sladjana Colakovic <74436050+SladjanaColakovic@users.noreply.github.com>
2021-04-18 02:48:51 +0200

    Uradjene notifikacije za uzimanje terapije

--- HospitalService/HospitalService/View/PatientUI/Pages/MedicalRecordWithPrescriptions.xaml.cs
+++ HospitalService/HospitalService/View/PatientUI/Pages/MedicalRecordWithPrescriptions.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -27,6 +28,10 @@ namespace HospitalService.View.PatientUI.Pages
         public List<Prescription> prescriptions { get; set; }
 
         public MedicalRecordStorage baza;
+
+        public Timer timer;
+        public Prescription p;
+        public String notifikacija = "";
         public MedicalRecordWithPrescriptions(Patient p)
         {
             InitializeComponent();
@@ -35,6 +40,40 @@ namespace HospitalService.View.PatientUI.Pages
             baza = new MedicalRecordStorage();
             prescriptions = baza.getByPatient(pacijent);
             tablePrescriptions.ItemsSource = prescriptions;
+        }
+
+        private void setReminder(object sender, RoutedEventArgs e)
+        {
+            
+            p = (Prescription)tablePrescriptions.SelectedItem;
+            if (p == null)
+            {
+
+                MessageBox.Show("Niste selektovali recept!");
+
+            }
+            else
+            {
+                
+                DateTime dt = p.start;
+                timer = new System.Timers.Timer(15000); // in milliseconds - p.HowOften*3600000
+                notifikacija += "Uzmite lijek" + " " + p.Medication.ToUpper() + "\n" + "Dodatne informacije: " + p.AdditionalInfos;
+                DateTime et = dt.AddDays(p.HowLong);
+                timer.Elapsed += (sender, e) => TimerMethod(sender, e, notifikacija, et);
+                timer.Start();
+               
+                
+            }
+
+        }
+
+        static void TimerMethod(object sender, ElapsedEventArgs e, string theString, DateTime et)
+        {
+            if (DateTime.Compare(DateTime.Now, et) < 0)
+            {
+                MessageBox.Show(theString);
+
+            }
         }
     }
 }
