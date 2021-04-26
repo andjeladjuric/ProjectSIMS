2d9681caa562d013cd879458ff480a574dc2c298
jovicpetra <jovic.petra@uns.ac.rs>
2021-04-26 17:30:13 +0200

    Kreirana uputnica

---
+++ HospitalService/HospitalService/Model/Referral.cs
@@ -0,0 +1,17 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HospitalService.Model
+{
+    class Referral
+    {
+        public DateTime DateOfIssue { get; set; }
+        public DoctorType Specialization { get; set; }
+        public Doctor Doctor { get; set; }
+        public bool Urgent { get; set; }
+
+        public Referral() { }
+    }
+}
